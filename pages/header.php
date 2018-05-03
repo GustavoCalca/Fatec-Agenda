840f6da7b23c37a28e942e57624383a5288048b6
Fernando <fernando@MacBook.local>
2018-05-03 09:24:56 -0300

    Implementação do bootstrap e fontawesome.

--- pages/header.php
+++ pages/header.php
@@ -3,11 +3,11 @@
 	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="http://agenda.local/css/vendor/bootstrap/bootstrap.css"/>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,200,400,600"/>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Anton:100,200,400,600"/>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" />
+	<link rel="stylesheet" href="http://agenda.local/css/vendor/fontawesome/fontawesome-all.css" />
 
-	<link rel="stylesheet" href="css/agenda.css" />
+	<link rel="stylesheet" href="http://agenda.local/css/agenda.css" />
 
 	<title>Agenda</title>
---
+++ pages/register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<?php
+	include_once('header.php');
+?>
+</head>
+
+<body class="back-register">
+
+
+<?php
+	include_once('js.php');
+?>
+</body>
+</html>
