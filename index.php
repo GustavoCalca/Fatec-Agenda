e3c19fb806f1ec59adff24fa1824d500c8e60d45
PKP <pkp@mailinator.com>
2018-06-06 21:03:24 -0300

    Atualização do dia 06/06

--- index.php
+++ index.php
@@ -3,53 +3,14 @@
 
 <head>
 <?php
-	include_once('pages/header.php');
+include_once('pages/header.php');
 ?>
 </head>
 
 <body class="back-page">
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#"><i class="fas fa-calendar-alt"></i> Agenda</a>
-			</div>
-
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="pages/login.php"><i class="fa fa-sign-in-alt" ></i> Login</a></li>
-					<li><a href="pages/register.php"><i class="fa fa-address-book"></i> Registrar-se</a></li>
-				</ul>
-			</div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid -->
-	</nav>
-
-	<!---------------------------------------------->
-	<!-- Este trecho será colocado em footer.php  -->
-	<!---------------------------------------------->
-	<footer class="main-footer footer-landpage">
-		<div class="pull-right hidden-xs">
-			<div style="text-align: right">
-				<strong>Curso de Análise e Desenvolvimento de Sistemas&nbsp;&nbsp;&nbsp;</strong>
-				<br>
-				<strong>Desenvolvido por: Prof. Me Fernando&nbsp;&nbsp;&nbsp;</strong>
-			</div>
-		</div>
-		<strong>
-			&nbsp;&nbsp;&nbsp;Fatec Taubaté
-		</strong>
-		<br>
-		&nbsp;&nbsp;&nbsp;Copyright &nbsp;&nbsp;&nbsp;&copy; <?php echo date('Y'); ?>. Todos os direitos reservados.
-	</footer>
-
 	<?php
+		include_once('pages/nav.php');
+		include_once('pages/footer.php');
 		include_once('pages/js.php');
 	?>
 </body>
--- pages/register.php
+++ pages/register.php
@@ -1,15 +1,70 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-<?php
-	include_once('header.php');
-?>
+	<?php include_once('header.php'); ?>
 </head>
 
 <body class="back-register">
+	<?php include_once('nav.php'); ?>
+
+	<div class="register-box">
+		<div class="register-box-body">
+			<div class="row" style="text-align: center">
+				<span class="register-title">
+					Sistema de Agenda
+				</span>
+			</div>
+			<hr class="hr-separator">
+			<div class="row" style="text-align: center">
+				<p class="register-subtitle">Forneça os dados solicitados</p>
+			</div>
+
+			<!-- Formulário que solicita os dados do usuário -->
+			<form action="insert_user.php" method="post" autocomplete="off">
+
+				<!-- nome do usuário -->
+				<div class="input-group">
+					<span class="input-group-addon" id="input-user-name">
+						<span class="fas fa-user"></span>
+					</span>
+					<input autocomplete="off" type="text" class="form-control"
+						name="user" required placeholder="Nome do Usuário"
+						aria-describedby="input-user">
+				</div>
+				<br>
+
+				<!-- e-mail do usuário -->
+				<div class="input-group">
+					<span class="input-group-addon" id="input-email">
+						<span class="fas fa-at"></span>
+					</span>
+					<input autocomplete="off" type="text" class="form-control"
+						name="email" required placeholder="E-mail do Usuário"
+						aria-describedby="input-email">
+				</div>
+				<br>
+
+
+			</form>
+
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <?php
+	include_once('footer.php');
 	include_once('js.php');
 ?>
 </body>
